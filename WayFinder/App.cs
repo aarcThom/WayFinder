b1aaba2ec830e5ee3891b4382b93830ae0b01e70
aarcThom <robertthomasgaudin@gmail.com>
2025-07-10 22:14:04 -0700

    Add project files.

---
+++ WayFinder/App.cs
@@ -0,0 +1,57 @@
+﻿using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Events;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Events;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+using System.Windows.Media.Imaging;
+
+namespace WayFinder
+{
+    public class App : IExternalApplication
+    {
+
+        // It's most reliable to store the UIapplication object from OnStartup as a class member
+        // Instead of creating a new instance each OnDocumentChanged
+        private static UIControlledApplication _controlledUIApp;
+
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            _controlledUIApp = application;
+
+            // ===================== SUBSCRIBE TO EVENTS ============================================================================================
+
+            try
+            {
+                // Subscribe to the DocumentOpened Event
+                _controlledUIApp.ControlledApplication.DocumentOpened += OnDocumentOpened;
+            }
+            catch (System.Exception)
+            {
+                return Result.Failed;
+            }
+
+            // ===================== RETURN ==========================================================================================================
+
+            return Result.Succeeded;
+        }
+
+
+        private void OnDocumentOpened(object sender, DocumentOpenedEventArgs e)
+        {
+            return;
+        }
+
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
